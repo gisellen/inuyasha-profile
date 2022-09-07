591bec8f2b181b8b7a124d7ef18fadc639a6a8a2
Gisellen <giselle.domingo.x@gmail.com>
2022-09-07 16:12:48 -0400

    WIP: Started homepage

---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.sass";
+
+export default function Home() {
+  return (
+    <div className="background-img">
+      <div className="blur">
+      <div className="text highlightDisable">犬夜叉</div>
+      </div>
+
+    </div>
+  );
+}
