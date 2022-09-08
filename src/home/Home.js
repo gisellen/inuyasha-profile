d7b25267f56e7a34449d8b21db2c64f3a2d50cdb
Gisellen <giselle.domingo.x@gmail.com>
2022-09-07 21:04:12 -0400

    WIP: mouse tracking for homepage movement

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./home.sass";
 
 export default function Home() {
+  const [offsetX, setOffsetX] = useState(0)
+  const [offsetY, setOffsetY] = useState(0)
+  const [speed, setSpeed] = useState(1/32)
+function handleMouseMove(e) {
+  console.log(e.pageX, e.pageY)
+  setOffsetX(e.pageX)
+}
+
+let offset = {
+  // backgroundPositionX: `${offsetX}`
+             }
   return (
-    <div className="background-img">
+    <div style={offset} onMouseMove={e => handleMouseMove(e)} className="background-img">
       <div className="blur">
       <div className="text highlightDisable">犬夜叉</div>
+      <div className="title">The Characters of Inuyasha</div>
       </div>
 
     </div>
