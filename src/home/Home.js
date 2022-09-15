84d03ee6c8a2963954a32868c078424d85d6918d
Gisellen <giselle.domingo.x@gmail.com>
2022-09-14 22:32:36 -0400

    Modified Background on Home Page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./home.sass";
 
 export default function Home() {
+  const [offsetX, setOffsetX] = useState(0)
+  const [offsetY, setOffsetY] = useState(0)
+  const [speed, setSpeed] = useState(50)
+function handleMouseMove(e) {
+  let _w = e.clientX - window.innerWidth/2
+  let _h = e.clientY - window.innerHeight/2
+  let depthX = `${50 + _w/speed}`
+  let depthY = `${50 + _h/speed}`
+  setOffsetX(depthX)
+  setOffsetY(depthY)
+}
+
+let offset = {
+  backgroundPosition: `${offsetX}% ${offsetY}%`
+}
+
   return (
-    <div className="background-img">
+    <div style={offset} onMouseMove={e => handleMouseMove(e)} className="background-img">
       <div className="blur">
       <div className="text highlightDisable">犬夜叉</div>
+      <div className="title"><span className="small-title">The Characters of</span> Inuyasha</div>
       </div>
 
     </div>
