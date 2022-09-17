e92b7f519590785fb9e4b2e5b0e56fe6e51a068f
Gisellen <giselle.domingo.x@gmail.com>
2022-09-17 17:09:33 -0400

    Updated routes

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 export default function Nav() {
   return (
     <nav className="nav">
-      <NavLink className="link" to="/home">HOME</NavLink>
+      <NavLink className="link" to="/">HOME</NavLink>
       <NavLink className="link" to="/about">ABOUT</NavLink>
     </nav>
   );
