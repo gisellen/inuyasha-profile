49071a49c8132000de621022953118d1fcc374b5
Gisellen <giselle.domingo.x@gmail.com>
2022-10-05 14:57:55 -0400

    Added different routing between home page and inuyasha page

--- src/home/Title.js
+++ src/home/Title.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Nav from "../common/Nav";
+import { NavLink } from "react-router-dom";
 
 export default function Title() {
-    const animation  = {
-        animate: { y: [-100, 0], opacity: [0, 1] },
-        exit: {opacity: 0, x:0, transition: {duration: 1}}
-    }
+  const animation = {
+    animate: { y: [-100, 0], opacity: [0, 1] },
+    exit: { opacity: 0, x: 0, transition: { duration: 1 } },
+  };
   return (
     <div className="title highlightDisable">
       <motion.span
@@ -24,7 +25,6 @@ export default function Title() {
         animate="animate"
         transition={{ delay: 0.9, ease: "easeOut", duration: 1 }}
         exit="exit"
-        
       >
         INUYASHA
       </motion.span>
@@ -34,7 +34,18 @@ export default function Title() {
         transition={{ delay: 3, ease: "easeOut", duration: 2 }}
         exit="exit"
       >
-        <Nav />{" "}
+        <Nav />
+      </motion.div>
+      <motion.div
+        className="inuyasha-link"
+        variants={animation}
+        animate={{ opacity: [0, 1] }}
+        transition={{ delay: 3, ease: "easeOut", duration: 2 }}
+        exit="exit"
+      >
+        <NavLink className="link" to="/inuyasha">
+          ENTER THE REALM
+        </NavLink>
       </motion.div>
     </div>
   );
