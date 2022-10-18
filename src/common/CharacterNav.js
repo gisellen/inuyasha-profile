cf6e76b62a09b5beb1167932294f3d71da1d517d
Giselle Ng <45869464+gisellen@users.noreply.github.com>
2022-10-18 18:50:03 -0400

    Added new page for kagome

---
+++ src/common/CharacterNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './common.sass'
+export default function CharacterNav() {
+  return (
+    <div className='center'>
+        <Link>inuyasha</Link>
+        <Link>kagome</Link>
+    </div>
+  )
+}
