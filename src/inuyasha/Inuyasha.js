2fe33f43bb25b9237f3589e38871c062ad9a13ba
Giselle Ng <45869464+gisellen@users.noreply.github.com>
2022-10-18 18:50:03 -0400

    Added new page for kagome

--- src/inuyasha/Inuyasha.js
+++ src/inuyasha/Inuyasha.js
@@ -3,7 +3,7 @@ import "./inuyasha.sass";
 import InuyashaImage from "../assets/Inuyasha.png";
 import InuyashaImage2 from "../assets/Inuyasha2.png";
 import { motion } from "framer-motion";
-
+import CharacterNav from "../common/CharacterNav";
 
 export default function Inuyasha() {
   const animation = {
@@ -11,26 +11,29 @@ export default function Inuyasha() {
     exit: { opacity: 0, x: 0, transition: { duration: 1 } },
   };
   return (
-    <motion.div 
-    variants={animation}
-    animate="animate"
-    className="inuyasha">
-      <img className="inu-pic2" src={InuyashaImage2} />
-      <section className="inuyasha-description">
-        <h2 className="inuyasha-header">INUYASHA</h2>
-        <p>
-          The son of a demon father and a human mother, was bound to a sacred
-          tree from Kikyo, the shrine priestess, while trying to claim the
-          Shikon Jewel, a powerful shard amongst demons. Until Kagome, who is supposedly Kikyo's
-          reincarnation, freed him.
-        </p>
-        <blockquote>"I'm a half-demon, more egotistical and greedy than any other living creature — that's what human beings are, right?"</blockquote >
-        <img className="inu-pic" src={InuyashaImage} />
-        
-      </section>
+    <motion.div variants={animation} animate="animate">
+      <div className="inuyasha">
+        <div className="inuyasha-card">
+          <img className="inu-pic2" src={InuyashaImage2} />
+          <section className="inuyasha-description">
+            <h2 className="inuyasha-header">INUYASHA</h2>
+            <p>
+              The son of a demon father and a human mother, was bound to a
+              sacred tree from Kikyo, the shrine priestess, while trying to
+              claim the Shikon Jewel, a powerful shard amongst demons. Until
+              Kagome, who is supposedly Kikyo's reincarnation, freed him.
+            </p>
+            <blockquote>
+              "I'm a half-demon, more egotistical and greedy than any other
+              living creature — that's what human beings are, right?"
+            </blockquote>
+            <img className="inu-pic" src={InuyashaImage} />
+          </section>
 
-
-        
+          
+        </div>
+        <CharacterNav />
+      </div>
     </motion.div>
   );
 }
---
+++ src/kagome/Kagome.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Kagome() {
+  return (
+    <div>Kagome</div>
+  )
+}
