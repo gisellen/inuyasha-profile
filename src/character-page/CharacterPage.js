1984a83836e8facde27d9046cc5a4a7356a4cc15
Gisellen <giselle.domingo.x@gmail.com>
2022-11-08 18:11:17 -0500

    Adjusted character page and animation

--- src/character-page/CharacterPage.js
+++ src/character-page/CharacterPage.js
@@ -6,34 +6,84 @@ import mirokuImg from "../assets/miroku.png";
 import shippoImg from "../assets/shippo.png";
 import kilalaImg from "../assets/kilala.png";
 
+import { Link } from "react-router-dom";
+
 import "./CharacterPage.sass";
 
 import { motion } from "framer-motion";
 
 export default function CharacterPage() {
+  const animation = {
+    animate: { opacity: [0, 1], transition: { duration: 2 } },
+    exit: { opacity: 0, x: 0, transition: { duration: 1 } },
+  };
+
   return (
-    <div className="center character-page">
-      <div className="character-container">
-        <div className="character">
-          <div className="character-title">inuyasha</div>
+    <motion.div
+      exit={{opacity: [1,0], transition: {duration: 1}}}
+      className="center character-page"
+    >
+      <motion.div
+        variants={animation}
+        animate="animate"
+        exit="exit"
+        className="character-container"
+      >
+        <Link to='/inuyasha' className="character">
+          <div className="character-title">
+            <div className="center" style={{flexDirection: 'column',}}>
+            <div>Inuyasha</div>
+            <div>犬夜叉</div>
+            </div>
+          </div>
           <img className="character-image" src={inuyashaImg} />
-        </div>
+        </Link>
         <div className="character">
+          <div className="character-title">
+          <div className="center" style={{flexDirection: 'column',}}>
+            <div>Kagome</div>
+            <div>かごめ</div>
+            </div>
+          </div>
           <img className="character-image" src={kagomeImg} />
         </div>
         <div className="character">
+          <div className="character-title">
+          <div className="center" style={{flexDirection: 'column',}}>
+            <div>Sango</div>
+            <div>珊瑚</div>
+            </div>
+          </div>
           <img className="character-image" src={sangoImg} />
         </div>
         <div className="character">
+          <div className="character-title">
+          <div className="center" style={{flexDirection: 'column',}}>
+            <div>Miroku</div>
+            <div>弥勒</div>
+            </div>
+          </div>
           <img className="character-image" src={mirokuImg} />
         </div>
         <div className="character">
+          <div className="character-title">
+          <div className="center" style={{flexDirection: 'column',}}>
+            <div>Shippo</div>
+            <div>七宝</div>
+            </div>
+          </div>
           <img className="character-image" src={shippoImg} />
         </div>
         <div className="character">
+          <div className="character-title">
+          <div className="center" style={{flexDirection: 'column',}}>
+            <div>Kilala</div>
+            <div>雲母</div>
+            </div>
+          </div>
           <img className="character-image" src={kilalaImg} />
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
